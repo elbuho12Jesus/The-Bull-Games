58cf12baf9b0cd0a7cef7fd2b76ff1f14b45b5b0
elbuho12Jesus <55631505+elbuho12Jesus@users.noreply.github.com>
2024-03-30 21:50:42 +0100

    files .cfg

---
+++ The-Bull-Games/Dependencies/Ogre/Samples/2.0/Tutorials/Tutorial_Terrain/include/Tutorial_TerrainGameState.h
@@ -0,0 +1,45 @@
+
+#ifndef _Demo_Tutorial_TerrainGameState_H_
+#define _Demo_Tutorial_TerrainGameState_H_
+
+#include "OgrePrerequisites.h"
+#include "TutorialGameState.h"
+
+namespace Ogre
+{
+    class Terra;
+    class HlmsPbsTerraShadows;
+    class TerraWorkspaceListener;
+}
+
+namespace Demo
+{
+    class Tutorial_TerrainGameState : public TutorialGameState
+    {
+        bool mLockCameraToGround;
+        float mTimeOfDay;
+        float mAzimuth;
+        Ogre::Terra *mTerra;
+        Ogre::Light *mSunLight;
+
+        /// Listener to make PBS objects also be affected by terrain's shadows
+        Ogre::HlmsPbsTerraShadows *mHlmsPbsTerraShadows;
+        Ogre::TerraWorkspaceListener *mTerraWorkspaceListener;
+
+        virtual void generateDebugText( float timeSinceLast, Ogre::String &outText );
+
+    public:
+        Tutorial_TerrainGameState( const Ogre::String &helpDescription );
+
+        Ogre::CompositorWorkspace* setupCompositor();
+
+        virtual void createScene01(void);
+        virtual void destroyScene(void);
+
+        virtual void update( float timeSinceLast );
+
+        virtual void keyReleased( const SDL_KeyboardEvent &arg );
+    };
+}
+
+#endif
